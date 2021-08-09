d3a82910af42352e25be72121666c35d464e6fc0
Ingo Richtsmeier <ingo.richtsmeier@camunda.com>
2021-08-09 14:17:28 +0200

    return healthPercentage as number, cleanup and add tests

--- CamundaApllication/src/test/java/com/example/workflow/WorkflowTest.java
+++ CamundaApllication/src/test/java/com/example/workflow/WorkflowTest.java
@@ -9,7 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.assertThat;
+import static org.assertj.core.api.Assertions.*;
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -23,29 +24,49 @@ public class WorkflowTest extends AbstractProcessEngineRuleTest {
     String processDefinitionKey = "Git-Repo-Popularity-Checker";
 
     ProcessInstance processInstance = runtimeService.createProcessInstanceByKey(processDefinitionKey)
-            .startBeforeActivity("Activity_0u7shp8")
+            .startBeforeActivity("Activity_0u7shp8" /*Get Repo Languages*/)
             .setVariable("repoOwner", "camunda")
             .setVariable("repoName", "camunda-bpm-platform")
             .execute();
 
-    assertThat(processInstance).isActive();
-
+    assertThat(processInstance).isActive()
+        .variables()
+          .contains(
+              entry("programingLanguages", "null Java  JavaScript  Python  Ruby "), 
+              entry("java", true),
+              entry("closure", false),
+              entry("javaScript", true),
+              entry("python", true),
+              entry("ruby", true),
+              entry("scala", false));
   }
-
+  
   @Test
-  public void shouldExecuteHappyPath() {
-    // given
-   // String processDefinitionKey = "Git-Repo-Popularity-Checker";
-
-    // when
-   // ProcessInstance processInstance = runtimeService.startProcessInstanceByKey(processDefinitionKey);
-
-    // then
-//    assertThat(processInstance).isStarted()
-//        .task()
-//        .hasDefinitionKey("say-hello")
-//        .hasCandidateUser("demo")
-//        .isNotAssigned();
+  public void testConnectorCallSuccesscully() {
+    ProcessInstance processInstance = runtimeService
+        .createProcessInstanceByKey("Git-Repo-Popularity-Checker")
+        .startBeforeActivity("Activity_0as6i8e" /*Get community profile*/)
+        .setVariables(withVariables(
+            "repoName", "camunda-bpm-platform", 
+            "repoOwner", "camunda"))
+        .execute();
+    
+    assertThat(processInstance).isWaitingAt("Activity_0scbww2" /*Display Results*/)
+        .variables().contains(entry("healthPercentage", Integer.valueOf(71)));
+  }
+  
+  @Test
+  public void testConnectorCallBpmnError() {
+    ProcessInstance processInstance = runtimeService
+        .createProcessInstanceByKey("Git-Repo-Popularity-Checker")
+        .startBeforeActivity("Activity_0as6i8e" /*Get community profile*/)
+        .setVariables(withVariables(
+            "repoName", "camunda-bpm-examples", 
+            "repoOwner", "camunda"))
+        .execute();
+    
+    assertThat(processInstance).isWaitingAt("Activity_0hbvr03" /*Look at error*/)
+        .variables().contains(entry("healthPercentage", Integer.valueOf(57)));
   }
 
 }
