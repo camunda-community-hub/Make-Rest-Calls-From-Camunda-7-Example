09da8a1a46910343dd201e8966c48ea42fd561cb
Ingo Richtsmeier <ingo.richtsmeier@camunda.com>
2021-08-10 21:00:26 +0200

    create a wire-mock server

---
+++ WireMockServer/java/src/main/java/org/camunda/devrel/WiremockServer.java
@@ -0,0 +1,26 @@
+package org.camunda.devrel;
+
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.*;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+
+public class WiremockServer {
+
+  public static void main(String[] args) {
+    WireMockServer wireMockServer = new WireMockServer(options().port(8089));
+    wireMockServer.start();
+    
+    wireMockServer.stubFor(get(urlEqualTo("/some/thing"))
+        .willReturn(okJson("{ \"message\": \"Hello\" }")));
+    
+    wireMockServer.stubFor(get(urlEqualTo("/delay"))
+        .willReturn(aResponse()
+            .withFixedDelay(10000)
+            .withHeader("Content-Type", "application/json")
+            .withBody("{ \"message\": \"this is 10 seconds late\" }")));
+    
+    wireMockServer.stubFor(get(urlEqualTo("/does/not/exist")).willReturn(notFound()));
+  }
+
+}
